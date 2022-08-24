919065115d153301950b55c2d0ebc883d53081d4
mitsuaki1229 <mitsuaki1229@gmail.com>
2022-08-24 20:58:15 +0900

    Init app.

---
+++ Bookshelf/Bookshelf/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bookshelf
+//
+//  Created by Mitsuaki Ihara on 2022/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
